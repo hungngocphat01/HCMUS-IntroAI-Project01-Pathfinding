3bbc100947562dcbca1043f6a58c7708befc7551
developerlmp <leminhphuc1804@gmail.com>
2021-11-13 00:04:54 +0700

    Thêm BFS

--- source/searching_algorithms.py
+++ source/searching_algorithms.py
@@ -3,6 +3,42 @@ from Stack import Stack
 from Queue import Queue
 from heuristic_func import *
 
+def BFS(graph: Graph, custom_start=None, custom_end=None):
+    # Kiểm tra người dùng có specify điểm bắt đầu và kết thúc tùy chỉnh hay không
+    # Phục vụ cho phần điểm thưởng
+    start_coord = custom_start if custom_start else graph.start
+    end_coord = custom_end if custom_start else graph.end
+     # Bộ nhớ fringe cho BFS là một queue
+    fringe = Queue()
+    # Tập đóng, chứa tọa độ các đỉnh đã đi qua
+    visited_coords = set()
+    # Đánh dấu node bắt đầu là đã đi
+    visited_coords.add(start_coord)
+    # Gắn node bắt đầu vào stack với chi phí 0
+    fringe.push(start_coord, 0)
+
+    while not fringe.is_empty():
+        current_node_coord, _ = fringe.pop()
+        successors = graph.get_successor(current_node_coord)
+        # Nếu là dead end: bỏ qua node này
+        if len(successors) == 0:
+            continue
+        # Kiểm tra từng node kế
+        for node in successors:
+            # Nếu node chưa viếng
+            if node['coord'] not in visited_coords:
+                # Thêm tọa độ của node vào tập đóng
+                visited_coords.add(node['coord'])
+                # Cập nhật lại node liền trước
+                graph.update_prev_node(node['coord'], current_node_coord)
+                # Thêm vào fringe
+                fringe.push(node['coord'], 0)
+            # Nếu node kế là đích đến: ồ yeah
+            if node['coord'] == end_coord:
+                return True
+            
+    return False
+    
 def DFS(graph: Graph, custom_start=None, custom_end=None):
     # Kiểm tra người dùng có specify điểm bắt đầu và kết thúc tùy chỉnh hay không
     # Phục vụ cho phần điểm thưởng
